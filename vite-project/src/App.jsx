a0fe81a5ee0fe7cb166fda05a30ae58c5cecef40
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-15 12:24:39 +0530

    component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import Card from "./component/card"
+const name ="Priyanshu"
 
 const App = () => {
   return (
     <div>
-      <h1>app</h1>
-      <img src="" alt="" />
-      <div>
-        hapy
+      <div className='card'>
+        <h1>Priyanshu Gupta</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, consequatur atque illum veritatis debitis maxime laudantium,cccadipisci dicta aut, sint nihil dolores optio!</p>
+        <h1>hiiiiiiiiiiiii</h1>
       </div>
+      <h1>Hi, my name is {name}</h1>
+      <Card />
+
     </div>
   )
 }
 
 export default App
- 
---
+++ vite-project/src/component/card.jsx
@@ -0,0 +1,6 @@
+function Card(){
+
+    return ("hi i'm a card")
+}
+
+export default Card;
