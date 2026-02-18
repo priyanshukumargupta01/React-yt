3bafb72819ae13233c225f7c73fdf3d028075bab
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-18 11:48:04 +0530

    more

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -4,9 +4,13 @@ import Card from './componenet/Card'
 
 const App = () => {
   return (
-    <div>
-      <Card />
+    
+    <div className="parent">
+      <Card user="Priya Rani" age={12} img="https://th.bing.com/th/id/OIP.IGWfpT_wKB3AyT-TAoNCbgHaEK?w=312&h=180&c=7&r=0&o=7&cb=defcache2&dpr=1.3&pid=1.7&rm=3&defcache=1"/>
+      <Card user="Priyanshu" age={22} img="https://th.bing.com/th/id/OIP.HAI6yARrc8rhYt4ezFUbqgHaEn?w=248&h=180&c=7&r=0&o=7&cb=defcache2&dpr=1.3&pid=1.7&rm=3&defcache=1"/> 
+      <Card user="Priyam" age={17} img="https://th.bing.com/th/id/OIP.AlfF7Y_KVhhky55JlD30zwHaE8?w=263&h=180&c=7&r=0&o=7&cb=defcache2&dpr=1.3&pid=1.7&rm=3&defcache=1"/>
     </div>
+    
   )
 }
 
--- Props/src/componenet/Card.jsx
+++ Props/src/componenet/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react' 
 
-const Card = () => {
+const Card = (props) => { 
   return (
-     
-      <div className="card">
-              <img src="https://th.bing.com/th/id/OIP.khwgAYyArCkpLh6lglOJ1gAAAA?w=236&h=420&c=7&o=5&cb=defcache2&dpr=1.3&pid=1.20&defcache=1" alt="" />
-              <h1>PriyanshuKumar</h1>
+      
+        <div className="card">
+              <img src={props.img} alt="" />
+              <h1>{props.user}</h1>
               <p>Lorem ipsum dolor sit amet, consectetur  8adipisicing elit. Ut minus alias nobis.</p>
-              <button>Click Me</button>
-            </div>
+              <button>View Profile</button>
+             
+      </div>
    )
 }
 
