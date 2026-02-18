aa3ccae28acb73a7a971bf361d6cecae636c2938
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-02-18 12:23:08 +0530

    more

---
+++ Cards-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './component/Card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+     <div className="card"> 
+    rtcyvubiu
+      
+      </div>
+    </div>
+  )
+}
+
+export default App
